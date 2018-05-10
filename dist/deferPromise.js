5b3f4e71fd6b93f8e43d096e5b01252fdee8d9a6
Edy <edi.luca@harrison.ro>
2018-05-10 15:51:15 +0300

    rebuilt dist

--- dist/deferPromise.js
+++ dist/deferPromise.js
@@ -1,23 +1,17 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 function deferPromise() {
-    var Promise = global.Promise;
-    var resolve, reject;
-    var promise = new Promise(function (_resolve, _reject) {
+    const Promise = global.Promise;
+    let resolve, reject;
+    const promise = new Promise((_resolve, _reject) => {
         resolve = _resolve;
         reject = _reject;
     });
     return {
-        then: function (f) { return promise.then(f); },
-        callback: function (err) {
-            var data = [];
-            for (var _i = 1; _i < arguments.length; _i++) {
-                data[_i - 1] = arguments[_i];
-            }
-            return err ? reject(err) : resolve(data);
-        },
-        promise: promise
+        then: f => promise.then(f),
+        callback: (err, ...data) => err ? reject(err) : resolve(data),
+        promise
     };
 }
-exports["default"] = deferPromise;
+exports.default = deferPromise;
 //# sourceMappingURL=deferPromise.js.map
--- dist/strategy.js
+++ dist/strategy.js
@@ -1,22 +1,12 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-exports.__esModule = true;
-var passport_strategy_1 = require("passport-strategy");
-var lodash_1 = require("lodash");
-var crypto = require("crypto");
-var deferPromise_1 = require("./deferPromise");
+Object.defineProperty(exports, "__esModule", { value: true });
+const passport_strategy_1 = require("passport-strategy");
+const lodash_1 = require("lodash");
+const crypto = require("crypto");
+const deferPromise_1 = require("./deferPromise");
 exports.defaultOptions = {
     queryExpiration: 86400,
-    passReqToCallback: false
+    passReqToCallback: false,
 };
 /**
  * `TelegramStrategy` constructor.
@@ -39,90 +29,85 @@ exports.defaultOptions = {
  *   User.findOrCreate({telegramId: user.id}, done);
  * });
  */
-var TelegramStrategy = /** @class */ (function (_super) {
-    __extends(TelegramStrategy, _super);
-    function TelegramStrategy(options, verify) {
-        var _this = _super.call(this) || this;
+class TelegramStrategy extends passport_strategy_1.Strategy {
+    constructor(options, verify) {
+        super();
         if (!options.botToken) {
             throw new TypeError('options.botToken is required in TelegramStrategy');
         }
         if (!verify) {
             throw new TypeError('LocalStrategy requires a verify callback');
         }
-        _this.options = lodash_1.assign({}, exports.defaultOptions, options);
-        _this.name = 'telegram';
-        _this.verify = verify;
-        _this.hashedBotToken = _this.botToken();
-        return _this;
+        this.options = lodash_1.assign({}, exports.defaultOptions, options);
+        this.name = 'telegram';
+        this.verify = verify;
+        this.hashedBotToken = this.botToken();
     }
-    TelegramStrategy.prototype.authenticate = function (req, options) {
-        var _this = this;
-        var query = req.method === 'GET' ? req.query : req.body;
+    authenticate(req, options) {
+        const query = req.method === 'GET' ? req.query : req.body;
         try {
-            var validationResult = this.validateQuery(req);
+            const validationResult = this.validateQuery(req);
             if (validationResult !== true)
                 return validationResult;
-            var promise = deferPromise_1["default"]();
+            const promise = deferPromise_1.default();
             if (this.options.passReqToCallback) {
                 this.verify(req, query, promise.callback);
             }
             else {
                 this.verify(query, promise.callback);
             }
-            promise.then(function (_a) {
-                var user = _a[0], info = _a[1];
+            promise.then(([user, info]) => {
                 if (!user)
-                    return _this.fail(info);
-                _this.success(user, info);
-            })["catch"](function (err) {
-                return _this.error(err);
+                    return this.fail(info);
+                this.success(user, info);
+            }).catch((err) => {
+                return this.error(err);
             });
         }
         catch (e) {
             return this.error(e);
         }
-    };
+    }
     /**
      * Function to check if provided date in callback is outdated
      * @returns {number}
      */
-    TelegramStrategy.prototype.getTimestamp = function () {
+    getTimestamp() {
         return parseInt((+new Date / 1000), 10);
-    };
+    }
     // We have to hash botToken too
-    TelegramStrategy.prototype.botToken = function () {
+    botToken() {
         // Use buffer to better performance
         return crypto.createHash('sha256').update(this.options.botToken).digest();
-    };
+    }
     /**
      * Used to validate if fields like telegram must send are exists
      * @param {e.Request} req
      * @returns {any}
      */
-    TelegramStrategy.prototype.validateQuery = function (req) {
-        var query = req.method === 'GET' ? req.query : req.body;
+    validateQuery(req) {
+        const query = req.method === 'GET' ? req.query : req.body;
         if (!query.auth_date || !query.hash || !query.id) {
             return this.fail({ message: 'Missing some important data' }, 400);
         }
-        var authDate = parseInt(query.auth_date);
+        const authDate = parseInt(query.auth_date);
         if (this.options.queryExpiration !== -1 &&
             (isNaN(authDate) || this.getTimestamp() - authDate > this.options.queryExpiration)) {
             return this.fail({ message: 'Data is outdated' }, 400);
         }
-        var sorted = Object.keys(query).sort();
-        var mapped = sorted // Everything except hash must be mapped
-            .filter(function (d) { return d !== 'hash' && d !== 'state'; })
-            .map(function (key) { return key + "=" + query[key]; });
-        var hashString = mapped.join('\n');
-        var hash = crypto
+        const sorted = Object.keys(query).sort();
+        const mapped = sorted // Everything except hash must be mapped
+            .filter(d => d !== 'hash' && d !== 'state')
+            .map(key => `${key}=${query[key]}`);
+        const hashString = mapped.join('\n');
+        const hash = crypto
             .createHmac('sha256', this.hashedBotToken)
             .update(hashString)
             .digest('hex');
         if (hash !== query.hash)
             return this.fail({ message: 'Hash validation failed' }, 403);
         return true;
-    };
-    return TelegramStrategy;
-}(passport_strategy_1.Strategy));
-exports["default"] = TelegramStrategy;
+    }
+}
+exports.default = TelegramStrategy;
 //# sourceMappingURL=strategy.js.map
